d134b3016b231eb31edcd9edb56bed3097f350f9
이지수 <devjisoolee@gmail.com>
2022-08-26 17:15:08 +0900

    test component

---
+++ src/testcomponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestComponent = () => {
+  return (
+    <>
+      <div>TestComponent</div>
+      <div style={{ backgroundColor: "red" }}>ddd</div>
+    </>
+  );
+};
+
+export default TestComponent;
