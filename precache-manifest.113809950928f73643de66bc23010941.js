08ad868e8b90aaa189cfd41854cc50d62b5246f9
이지수 <devjisoolee@gmail.com>
2022-08-26 17:25:01 +0900

    Updates

---
+++ precache-manifest.113809950928f73643de66bc23010941.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4b87b3ff66b00969eb427e56f099ed",
+    "url": "/testgrapes/index.html"
+  },
+  {
+    "revision": "82fd979cfe07388b9c80",
+    "url": "/testgrapes/static/css/main.aaee291f.chunk.css"
+  },
+  {
+    "revision": "396e273b665864a1f9d6",
+    "url": "/testgrapes/static/js/2.5cab2f2d.chunk.js"
+  },
+  {
+    "revision": "290bd141c9c7dd9dede7c43e2c6f62c4",
+    "url": "/testgrapes/static/js/2.5cab2f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fd979cfe07388b9c80",
+    "url": "/testgrapes/static/js/main.b196deb9.chunk.js"
+  },
+  {
+    "revision": "c221033e511726b64b75",
+    "url": "/testgrapes/static/js/runtime-main.f9b12de1.js"
+  },
+  {
+    "revision": "38b30fa0229248d8bd3a14c802046743",
+    "url": "/testgrapes/static/media/main-fonts.38b30fa0.ttf"
+  },
+  {
+    "revision": "638868fe8f33b0b45c29fc4e4fba99a9",
+    "url": "/testgrapes/static/media/main-fonts.638868fe.woff"
+  },
+  {
+    "revision": "eceff26a5f33cc81123f437a16a78dbe",
+    "url": "/testgrapes/static/media/main-fonts.eceff26a.svg"
+  },
+  {
+    "revision": "f7c56e52c2c44690a0e39d5a486d0c3b",
+    "url": "/testgrapes/static/media/main-fonts.f7c56e52.eot"
+  }
+]);
